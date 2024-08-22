bff8994aa7c297b54439245491eb49cf5f082732
Rahul Barve <rahulsbarve@gmail.com>
2024-08-22 11:47:34 +0530

    Adding files to Repo

--- git-init/repositories.tf
+++ git-init/repositories.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "my-terraform-created-repo" {
+resource "github_repository" "my-terraform-created-repo-with-actions" {
   name        = "my-terraform-created-repo-with-actions"
   description = "Terraform created repo"
 
@@ -9,23 +9,24 @@ resource "github_repository" "my-terraform-created-repo" {
 }
 
 resource "github_branch" "development" {
-    repository = github_repository.my-terraform-created-repo.name
+    repository = github_repository.my-terraform-created-repo-actions.name
     branch = "development"
+    source_branch = "main"
 }
 
 resource "github_branch_default" "default" {
-    repository = github_repository.my-terraform-created-repo.name
-    branch = github_branch.development.branch
+    repository = github_repository.my-terraform-created-repo-actions.name
+    branch = github_branch.development.source_branch
 }
 
-resource "github_branch_protection" "branch_enterprise" {
-    repository_id = github_repository.my-terraform-created-repo.node_id
+# resource "github_branch_protection" "branch_enterprise" {
+#     repository_id = github_repository.my-terraform-created-repo-actions.node_id
 
-    pattern = github_branch.development.branch
-    enforce_admins = true
+#     pattern = github_branch.development.branch
+#     enforce_admins = true
 
-    required_pull_request_reviews {
-      dismiss_stale_reviews = true
-      restrict_dismissals = true
-    }
-}
+#     required_pull_request_reviews {
+#       dismiss_stale_reviews = true
+#       restrict_dismissals = true
+#     }
+# }
