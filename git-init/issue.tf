98232508bef4b70c7a08afd5a363c743673d330c
Rahul Barve <rahulsbarve@gmail.com>
2024-08-22 12:50:34 +0530

    update file

--- git-init/issue.tf
+++ git-init/issue.tf
@@ -1,5 +1,5 @@
 resource "github_issue" "tf_issues" {
-  repository = github_repository.my-terraform-created-repo.name
+  repository = github_repository.my-terraform-created-repo-actions.name
   title = "New Issue"
   body = "There are fresh issues created from Terraform"
 }
